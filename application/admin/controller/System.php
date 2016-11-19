4a2e09bcc4b2c063b337337465614eaf92884ac1
ayflying <ay@7cuu.com>
2016-11-19 23:17:38 +0800

    静态配置文件config可以直接在后台进行修改

--- application/admin/controller/System.php
+++ application/admin/controller/System.php
@@ -1,26 +1,36 @@
 <?php
 namespace app\admin\controller;
 use think\Config;
+use think\Db;
+use think\Cache;
 
 class System extends Common{
     public function index(){
 		$mod = input('get.mod');
-		$db = db('SystemSettings');
-		$file = "./Config/common.php";
-		//$config = include($file);//读取本地配置文件
-		//$this -> assign('config',$config);
-		
+		$db = Db::name('SystemSettings');
+		$file = "./config.php";
+        $config = file_get_contents($file);
+        
 		if(request()->isPost()){
-			$arr = input('post.');
-			//$db->create();
-			unset($arr['button']);
-			unset($arr['config']);
-			foreach($arr as $k => $val){
+			$post = input('post.');
+            
+            //修改本地配置文件
+            foreach($post['config'] as $key => $val){
+                if(constant($key) != $val){
+                    $config = str_replace(constant($key), $val, $config);
+                    file_put_contents($file,$config);
+                }
+            }
+			unset($post['config']);
+			foreach($post as $k => $val){
 				//$db -> value = $val;
-				$db -> where(["name" => $k]) -> setField('value',$val);//update(['vlaue' => $val]);
+				$db -> where("name", $k) -> setField('value',$val);//update(['vlaue' => $val]);
 			}
+            
+            
+            
 			/*
-			//修改本地配置文件
+			
 			$config = input('post.config');
 			$word = "<?php\r\nreturn array(\r\n";
 			foreach($config as $key => $val){
@@ -30,22 +40,29 @@ class System extends Common{
 			file_put_contents($file, $word);
 			//修改本地配置文件
 			*/
-			return  $this->success('修改成功','','',1);
+			return  $this->success('修改成功',null,null,1);
 			//$this -> cache();
 		}else{
 			$sql = $db -> where("class > '0'") -> select();
-			
 			foreach($sql as $k => $val){
 				//dump($val);
 				$sql[$k]['value'] = htmlspecialchars_decode($val['value']);
 				$system[$val['name']] = $val;
 			}
-			$this -> assign('sql',$sql);
-			//dump($sql);
+			
+            
+            //读取本地配置文件
+            preg_match_all('/define\([\'|\"]?(.*?)[\'|\"]?,[\'|\"]?(.*?)[\'|\"]?\)/',$config,$arr);
+            $config = [];
+            foreach($arr[2] as $key => $val){
+                $config[$arr[1][$key]] = $val;
+            }
+            
+            $this -> assign('sql',$sql);
+            $this -> assign('config',$config);
 			return $this->fetch('/system');
 			
 		}
-		
     }
 	
 	
@@ -62,7 +79,7 @@ class System extends Common{
 		
 		if(isset($dir)){
 			//$dir = input('get.dir');
-			db('system_settings') -> where(['name' => 'theme']) -> update(['value' => $dir]);
+			Db::name('system_settings') -> where(['name' => 'theme']) -> update(['value' => $dir]);
 			//修改首页配置文件
 			$config = file_get_contents('./config.php');
 			//$config = strtr($config,VIEW_PATH,'/template/'.$dir.'/');
@@ -78,7 +95,7 @@ class System extends Common{
 	}
 	
 	public function cache(){
-		\think\Cache::clear();
+		Cache::clear();
 		$this -> deldir(CACHE_PATH);		//CACHE_PATH 项目模板缓存目录（默认为 RUNTIME_PATH.'cache/'）
 		$this -> deldir(TEMP_PATH);		//TEMP_PATH 应用缓存目录（默认为 RUNTIME_PATH.'temp/'）
 		
--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
 define('VIEW_PATH','./template/default/');
-define('PAGE_NUM',50);
+define('PAGE_NUM',80);
 define('UPDATE_URL','http://bbcms.hasee.7cuu.com');
